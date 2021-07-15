e3b4758277f9c7092822ac6279476affb4820123
Haghani <vhaghani@ucdavis.edu>
2021-07-15 16:23:51 -0700

    add comments

--- Viki/hello_world.c
+++ Viki/hello_world.c
@@ -2,3 +2,12 @@
 int main() {
 	printf("hello world\n");
 }
+
+/*
+	Here I am making a multi-line comment.
+    A multi-line comment begins with slash-star.
+	It ends with star-slash.
+*/
+
+/* This is how to make a single-line comment the old way */
+// This is the new version of a single-line comment
