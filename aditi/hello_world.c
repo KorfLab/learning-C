142de9819acc126edac7e4a555d24c3ddda13a69
aditi-goyal-523 <adigoyal@ucdavis.edu>
2021-07-15 17:09:01 -0700

    adding my directory

---
+++ aditi/hello_world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	printf("hello world\n");
+}
+
+// once a program is compiled, it can be run multiple times
+
+/* if you make a change to a program, 
+you will need to recompile it, and rename it.
+the default name is a.out.
+the name can be the same as the name before 
+ie, if I'm debugging the program, greeting, I can rename it greeting
+then you compile and run
+*/
+
---
+++ aditi/variables.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int a;     // declaring a variable
+	int b, c;  // declaring multiple variables of the same type
+	int d = 0; // declarating and assigning at the same time
+	printf("%d %d %d %d\n", a, b, c, d); // %d for digit
+
+	// char is a character and also an 8-bit integer
+	char nt = 'A'; // single characters are in single quotes
+	printf("%c %d\n", nt, nt); // ASCII value of A is 65
+
+	// %c for char
+
+	// there are many flavors of integers
+	char i1;           // -128 to 127
+	unsigned char i2;  // 0 to 255
+	short i3;          // -32768 to 32767
+	unsigned short i4; // 0 to 65536
+	long i5;           // depends, see limits.h
+	unsigned long i6;  // depends, see limits.h
+
+	// there are two common flavors of floats
+	float f1 = M_PI;       // 1.2e-38 to 3.4e+38
+	double f2 = M_PI;      // 2.3e-308 to 1.7e+308
+	printf("%f %f\n", f1, f2);
+	printf("%.3f\n", f2);  // limiting precision
+
+	// there are even more types of variables!
+
+	// Ian's play area
+	float wtf1;
+	double wtf2;
+	double pi2 = 2 * M_PI;
+	printf("%f %f %f\n", wtf1, wtf2, pi2);
+	
+}
+
