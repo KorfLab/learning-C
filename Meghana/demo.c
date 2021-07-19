955188d6cfc1d38aa785c11452cec74214ebde4f
meghana202 <meghymail@gmail.com>
2021-07-19 11:28:39 -0700

    new

---
+++ Meghana/demo.c
@@ -0,0 +1,9 @@
+#include "biosequence.h"
+
+int main(int argc, char **argv) {
+	BioSeq s1 = bs_new("EcoRI", "GAATTC");
+	bs_set_line_length(3);
+	bs_print(s1);
+	bs_free(s1);
+}
+
---
+++ Meghana/fizzbuzz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	for (int i = 1; i <= 100; i++) {
+		/*if (i%3==0 & i%5==0) printf("%d fizzbuzz\n", i);
+		else if(i%3==0) printf("%d fizz\n", i);
+		else if (i%5==0) printf("%d buzz\n", i);*/
+		if (i%3==0 & i%5==0) printf("fizzbuzz\n");
+		else if(i%3==0) printf("fizz\n");
+		else if (i%5==0) printf("buzz\n");
+	}
+}
+			
