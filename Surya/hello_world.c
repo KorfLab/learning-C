3cfa07698cff7146fbf311eef8dd454078ae6e16
surya-vishnubhatt <spvishnubhatt@ucdavis.edu>
2021-07-28 12:53:59 -0700

    makin sum pancakes

--- Surya/hello_world.c
+++ Surya/hello_world.c
@@ -1,4 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> //import functions
 int main() {
-	printf("hello world\n");
+	printf("hello world\n"); //include newline character to get new line, no print line function, just printf
+	//all statements end in semicolons
 }
+//main returns an integer value, returns number if no work, 0 if works
+//if everything works normal exit 0, error = > exit 1
+//
